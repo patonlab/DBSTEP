7a2d65ed119cb2d0a2115b8b4bd477498415b5f5
toby <tjpatt@rams.colostate.edu>
2021-06-01 15:41:37 -0600

    changed GetXYZData to XYZParser which inherits from DataParser

--- dbstep/Dbstep.py
+++ dbstep/Dbstep.py
@@ -79,7 +79,7 @@ class dbstep:
 		elif ext == 'rdkit':
 			mol = parse_data.GetData_RDKit(name, options.noH, options.spec_atom_1, options.spec_atom_2)
 		elif ext in [".xyz",'.com','.gjf']:
-			mol = parse_data.GetXYZData(name, ext, options.noH,options.spec_atom_1, options.spec_atom_2)
+			mol = parse_data.XYZParser(file, ext[1:], options.noH,options.spec_atom_1, options.spec_atom_2)
 			if options.noH:
 				options.spec_atom_1 = mol.spec_atom_1
 				options.spec_atom_2 = mol.spec_atom_2
